454b2299b945310834e70724a4247435f33773fe
iceStone <ice@micua.com>
2016-12-23 01:25:20 +0800

    demo test

--- src/test/models/user.js
+++ src/test/models/user.js
@@ -4,33 +4,33 @@ import { User } from '../../server/models'
 const users = []
 
 test.before(async t => {
-  const user = await User.add('zce', 'ice@wedn.net', '12345689', 'iceStone', 'zce', '13241087977', 'administrator', 'activated')
+  const user = await User.add('zce-demo', 'demo@wedn.net', '12345689', 'Stone', 'zce-demo', '13241087977', 'administrator', 'activated')
   users.push(user)
 })
 
 test('models.user.getBySlug', async t => {
-  const user = await User.getBySlug('zce')
-  t.is(user.nickname, 'iceStone')
+  const user = await User.getBySlug('zce-demo')
+  t.is(user.nickname, 'Stone')
 })
 
 test('models.user.getByUsername', async t => {
-  const user = await User.getByUsername('zce')
-  t.is(user.nickname, 'iceStone')
+  const user = await User.getByUsername('zce-demo')
+  t.is(user.nickname, 'Stone')
 })
 
 test('models.user.getByEmail', async t => {
-  const user = await User.getByEmail('ice@wedn.net')
-  t.is(user.nickname, 'iceStone')
+  const user = await User.getByEmail('demo@wedn.net')
+  t.is(user.nickname, 'Stone')
 })
 
 test('models.user.getByMobile', async t => {
   const user = await User.getByMobile('13241087977')
-  t.is(user.nickname, 'iceStone')
+  t.is(user.nickname, 'Stone')
 })
 
 test('models.user.getByUnique', async t => {
-  const user1 = await User.getByUnique('zce')
-  const user2 = await User.getByUnique('ice@wedn.net')
+  const user1 = await User.getByUnique('zce-demo')
+  const user2 = await User.getByUnique('demo@wedn.net')
   const user3 = await User.getByUnique('13241087977')
   t.is(user1.nickname, user2.nickname)
   t.is(user2.nickname, user3.nickname)
@@ -38,7 +38,7 @@ test('models.user.getByUnique', async t => {
 
 test('models.user.add', async t => {
   const username = Date.now()
-  const user = await User.add('a' + username, username + '@wedn.net', '12345678')
+  const user = await User.add('a' + username, username + 'demo@wedn.net', '12345678')
   t.not(user.id, 0)
   users.push(user)
 })
@@ -48,10 +48,16 @@ test('models.user.add2', async t => {
   const user = await User.add({
     username: 'a' + username,
     password: '12345678',
-    email: username + '@wedn.net'
+    email: username + 'demo@wedn.net'
   })
   t.not(user.id, 0)
   users.push(user)
+})
+
+test('models.user.demo', async t => {
+  const user = await User.getByUsername('zce-demo')
+  user.set('slug', 'world')
+  console.log(user)
 })
 
 test.after(async t => {
