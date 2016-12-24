398d1fa50d0bc70fb38468d9b1a19ed953af0eff
iceStone <ice@micua.com>
2016-12-24 15:08:55 +0800

    no migration && db init model

---
+++ src/server/models/init.js
@@ -0,0 +1,28 @@
+import { hash } from '../libraries/encrypt'
+
+export default async init (db) {
+  await db.models.Option.bulkCreate([
+    { key: 'site_name', value: 'WEDN.NET' },
+    { key: 'site_description', value: 'make IT better' },
+    { key: 'site_favicon', value: '/favicon.ico' },
+    { key: 'site_lang', value: 'zh-CN' },
+    { key: 'site_theme', value: '2016' }
+  ])
+
+  await db.models.User.create({
+    slug: 'zce',
+    username: 'zce',
+    password: await hash('5love100'),
+    nickname: 'iceStone',
+    email: 'zce@wedn.net',
+    mobile: '13241087977',
+    status: 'activated',
+    role: 'administrator',
+    Meta: [
+      { key: 'first_name', value: 'Lei' },
+      { key: 'last_name', value: 'Wang' }
+    ]
+  }, {
+    include: [db.models.User.associations.Meta]
+  })
+}
